321bef5ac643e4ff774e48fb126f7bc7774d69ca
fukubaka <takafk9@yahoo.co.jp>
2018-12-16 07:18:32 +0000

    ルーティングを修正 questioncontrollerコントローラのbyebugへのコメント修正
    ユーザー認証機能実装に必要なため

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'answers/edit'
-  # get 'questions/index'
-  # get 'questions/show'
-  # get 'questions/new'
-  # get 'questions/edit'
+ get 'mypage' ,to: 'users#me'
+ get 'login' ,to: 'session#create'
+ get 'logout' ,to: 'session#destroy'
   # ルートパスにアクセスでindexアクション
   root 'questions#index'
   # 必要なルーティング用意
   resources :questions do
     resources :answers
   end
+  resources :users, only: %i[new create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
